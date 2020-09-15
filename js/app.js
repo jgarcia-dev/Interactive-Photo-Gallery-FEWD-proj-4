e039d7e45fc49e7b5a716eafa17bd4e191d3c9a6
John Garcia <johngarciadev@gmail.com>
2020-09-15 10:58:51 -0700

    Updated variable name and comments

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ function search() {
 
     userInput = document.getElementById("search-bar");
     filteredInput = userInput.value.toLowerCase();
-    galleryPics = document.getElementById("gridGallery").getElementsByTagName("a");
+    galleryPics = document.getElementById("grid").getElementsByTagName("a");
    
     // for every caption text in gallery pictures
-    // check caption against user input, if match show picture, else hide
+    // check caption against filtered input, if match show picture, else hide
     for (i = 0; i < galleryPics.length; i++) {
         
         captionTxt = galleryPics[i].getAttribute("data-caption");
